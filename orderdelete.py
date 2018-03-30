2889a49646b27389b9409abd182b3b3d527def03
Junior Corman <junior.corman@gmail.com>
2018-03-29 20:01:57 -0500

    Refactoring, suporting python3

--- orderdelete.py
+++ orderdelete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 # Funcion que Ordena y Elimina los duplicados
 def ordered_set(in_list):
     out_list = []
@@ -11,18 +12,20 @@ def ordered_set(in_list):
     out_list.sort()
     return out_list
 
+
 # Funcion principal: captura los datos
 def run():
     # myList = [10,8,1,1,3,6,2]
     # print(myList)
     # print(ordered_set(myList))
 
-    input = str(raw_input('Ingresa los datos, separado por espacios: ')).split()
+    data = input('Ingresa los datos, separado por espacios: ').split()
 
-    input = list(map(int, input))
+    data = list(map(int, data))
 
-    print(ordered_set(input))
+    print(ordered_set(data))
+
 
 # Nuestro programa empieza aqui.
 if __name__ == '__main__':
-	run()
+    run()
